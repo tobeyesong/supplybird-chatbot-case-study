3a3f0af6ee4abe127a8d35ef9b3a572332e80fd7
Toan Lam <toanlam01@gmail.com>
2026-05-26 21:28:50 -0700

    Submit chat leads to Netlify form path

--- src/components/chatbot-embed.tsx
+++ src/components/chatbot-embed.tsx
@@ -42,7 +42,7 @@ export function ChatbotEmbed() {
     })
 
     try {
-      const response = await fetch('/', {
+      const response = await fetch('/netlify-forms.html', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: body.toString(),
